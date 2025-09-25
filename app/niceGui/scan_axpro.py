0235b1fb8d0d99fec3922d46e60f8c32e3816e00
Robert Milicevic <milicevicrobert@gmail.com>
2025-09-25 12:09:52 +0200

    test ispis df

--- app/niceGui/scan_axpro.py
+++ app/niceGui/scan_axpro.py
@@ -33,6 +33,8 @@ def poll_zones_df(cookie) -> pd.DataFrame:
     zone_list = [z["Zone"] for z in data.get("ZoneList", [])]
     df = pd.DataFrame(zone_list, columns=["id", "name", "alarm"])
     df["alarm"] = df["alarm"].apply(lambda x: int(x) == 1 if x is not None else False)
+    #test makni kasnije
+    print(df)
     return df[df["alarm"]].reset_index(drop=True)
 
 
